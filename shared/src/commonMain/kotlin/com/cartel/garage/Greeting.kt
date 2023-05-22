dadc58a66726b18bfbc789840e441c85075396a9
Franz <softymodel@gmail.com>
2023-05-22 10:38:49 +0300

    Impl for functionality of bl

--- shared/src/commonMain/kotlin/com/cartel/garage/Greeting.kt
+++ shared/src/commonMain/kotlin/com/cartel/garage/Greeting.kt
@@ -6,9 +6,4 @@ import io.ktor.client.statement.*
 
 class Greeting {
     private val client = HttpClient()
-
-    suspend fun greeting(): String {
-        val response = client.get("https://ktor.io/docs/")
-        return response.bodyAsText()
-    }
 }
---
+++ shared/src/commonMain/kotlin/com/cartel/garage/functionality/Get_Post_Querry.kt
@@ -0,0 +1,25 @@
+package com.cartel.garage.functionality
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+
+class Get_Post_Querry {
+    private val client= HttpClient()
+    private val URL="127.0.0.1:8080"
+
+    fun post_login_page(){
+        //client.get {  }
+    }
+    fun get_profile_page(){
+
+    }
+    fun get_head_page(){
+
+    }
+    fun get_match_page(){
+
+    }
+    fun get_stock_page(){
+
+    }
+}
